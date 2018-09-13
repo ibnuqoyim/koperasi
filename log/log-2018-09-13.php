1101031df90cd360890ee49cd99f3076dc2a07d8
ibnuqoyim <muhamadibnu9@gmail.com>
2018-09-13 09:37:33 +0700

    first commit

---
+++ log/log-2018-09-13.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2018-09-13 09:28:29 --> Severity: Warning  --> mysqli_connect(): (HY000/1049): Unknown database 'db_koperasi' C:\xampp\htdocs\koperasi\system\database\drivers\mysqli\mysqli_driver.php 77
+ERROR - 2018-09-13 09:28:29 --> Unable to connect to the database
